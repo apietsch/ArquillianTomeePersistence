11246f1f6feed7ed02a51209d41715b8a6416f7c
Helge Waastad <helge.waastad@datametrix.no>
2014-09-04 21:01:50 +0200

    Working version :-)

--- src/test/java/org/waastad/arquillianpersistence/RepositoryTest.java
+++ src/test/java/org/waastad/arquillianpersistence/RepositoryTest.java
@@ -10,13 +10,14 @@ import java.util.List;
 import javax.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.persistence.PersistenceTest;
+import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.persistence.UsingDataSet;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.waastad.arquillianpersistence.entity.UserAccount;
@@ -29,7 +30,6 @@ import org.waastad.arquillianpersistence.service.UserService;
  * @author Helge Waastad <helge.waastad@datametrix.no>
  */
 @RunWith(Arquillian.class)
-@PersistenceTest
 public class RepositoryTest {
 
     @Deployment(testable = true)
@@ -37,30 +37,42 @@ public class RepositoryTest {
         File[] libs = Maven.resolver().loadPomFromFile("pom.xml").importRuntimeDependencies().resolve().withTransitivity().asFile();
         return ShrinkWrap.create(WebArchive.class, "test.war")
                 .addClasses(UserService.class, EntityManagerProducer.class, UserAccountRepository.class, UserAccount.class)
-//                .addClasses(AbstractEntityRepository.class, Repository.class,
-//                        EntityRepository.class,
-//                        FirstResult.class, MaxResults.class, Modifying.class,
-//                        Query.class, QueryParam.class, QueryResult.class,
-//                        EntityManagerConfig.class, EntityManagerResolver.class)
-//                .addClasses(Criteria.class, QuerySelection.class, CriteriaSupport.class)
-//                .addClasses(CreatedOn.class, CurrentUser.class, ModifiedBy.class, ModifiedOn.class)
-//                .addClasses(MappingConfig.class, QueryInOutMapper.class)
-//                .addClasses(DelegateQueryHandler.class, QueryInvocationContext.class)
-                //                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
-                //                .addAsManifestResource("test-persistence.xml", "persistence.xml")
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
                 .addAsWebInfResource("test-persistence.xml", "persistence.xml")
                 .addAsLibraries(libs);
     }
 
+//    @Deployment(testable = true)
+//    public static Archive<?> createDeploymentPackageJar() {
+//        JavaArchive[] libs = Maven.resolver().loadPomFromFile("pom.xml").importRuntimeDependencies().resolve().withTransitivity().as(JavaArchive.class);
+//        JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "test.jar")
+//                .addClasses(UserService.class, EntityManagerProducer.class, UserAccountRepository.class, UserAccount.class)
+//                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+//                .addAsManifestResource("test-persistence.xml", "persistence.xml");
+//        for (JavaArchive lib : libs) {
+//            archive = archive.merge(lib);
+//        }
+//        return archive;
+//    }
     @Inject
     private UserService userService;
+    @Inject
+    UserAccountRepository userAccountRepository;
 
     @Test
-    @UsingDataSet("datasets/users.yml")
+    @UsingDataSet("users.yml")
+    @InSequence(value = 1)
     public void testSomeMethod() {
         List<UserAccount> users = userService.getUsers();
-        System.out.println("Testing......" + users.size());
+        Assert.assertEquals(2,users.size());
+    }
+    
+    @Test
+    @UsingDataSet("users.yml")
+    @InSequence(value = 2)
+    public void testSomeMethod2() {
+        UserAccount findBy = userAccountRepository.findBy(1L);
+        Assert.assertEquals("Frank",findBy.getFirstname());
     }
 
 }
